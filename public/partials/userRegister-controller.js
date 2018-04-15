08d6287d0b80a5c77c227b45c03fbbdb7948fe0b
unknown <gustavo.h.militao@gmail.com>
2018-04-15 00:48:32 -0300

    Implementação de ajustes na tela de cadastro de cliente.

--- public/partials/userRegister-controller.js
+++ public/partials/userRegister-controller.js
@@ -1,17 +1,16 @@
 angular.module('transcota')
-	.controller('customerRegisterController',
+	.controller('userRegisterController',
 
 	function ($scope, user) {
 		$scope.nome = "";
 		$scope.ativo = false;
-		$scope.documento = "";
+		$scope.cpf = "";
 		$scope.rg = "";
 		$scope.perfil = "";
 		$scope.login = "";
 		$scope.senha = "";
 		$scope.email = "";
 		$scope.observacao = "";
-		$scope.tipoCliente = "0";
 
 		$scope.submit = function () {
 			$('.btn-load').button('loading');
